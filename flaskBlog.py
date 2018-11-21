0b25d0d9ec29f370ca6cef19ff5174000b55895c
Perry K <apk29@hotmail.com>
2018-11-21 00:29:41 -0800

    forms, error msg

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '24297ef8105a8776d669e97281a316c6'
 
 posts = [
     {
@@ -24,6 +27,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='about') #this title equals the word 'about', instead of the array
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
